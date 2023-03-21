5260d6f8ed1b967c5f4dfe7dadd68f1334396029
Georgi Georgiev <auratiger00@gmail.com>
2023-03-21 14:28:13 +0200

    feat: desing specs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,16 @@
+const colors = {
+  "primary-marine": "hsl(213, 96%, 18%)",
+  "primary-purple": "hsl(243, 100%, 62%)",
+  "primary-pastel": "hsl(228, 100%, 84%)",
+  "primary-light": "hsl(206, 94%, 87%)",
+  "secondary-strawberry": "hsl(354, 84%, 57%)",
+  "secondary-cool": "hsl(231, 11%, 63%)",
+  "secondary-light": "hsl(229, 24%, 87%)",
+  "secondary-magnolia": "hsl(217, 100%, 97%)",
+  "secondary-alabaster": "hsl(231, 100%, 99%)",
+  "secondary-white": "hsl(0, 0%, 100%)",
+};
+
 module.exports = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx}",
@@ -6,10 +19,21 @@ module.exports = {
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    screens: {
+      sm: "375px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1440px",
+    },
     extend: {
       fontFamily: {
         ubuntu: ["var(--font-ubuntu)"],
       },
+
+      backgroundColor: colors,
+      textColor: colors,
+      borderColor: colors,
     },
   },
   plugins: [],
